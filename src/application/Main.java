1c7a1efbf09c307e56839b60202b2908be662c0c
Jhéssica Moura <jhessica.moura7@gmail.com>
2022-03-08 11:26:20 -0300

    Refactored and finished project

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,22 +1,26 @@
 package application;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		ArrayList<String> frases = new ArrayList<String>();
-		frases.add("As pessoas costumam dizer que a motivação não dura sempre. Bem, nem o efeito do banho, por isso recomenda-se diariamente");
-		frases.add("Sacode a poeira e dá a volta por cima! Só tome cuidado com a rinite.");
-		frases.add("Vá atrás dos seus objetivos com a mesma agilidade que você corre para o banheiro quando está apertado.");
-		frases.add("Se pizza que vem de moto demora, imagine quanto tempo seus sonhos vão demorar pra chegar se você não sair do lugar!");
-		frases.add("Se a vida te der uma rasteira, pegue a coberta, tire um cochilo e depois vá em busca dos seus sonhos.");
-		frases.add("Viva como se fosse morrer, porque você vai.");
-		
-		int fraseAleatoria = (int) (Math.random() * frases.size());
-		
-		System.out.println("E a frase do dia é: " + frases.get(fraseAleatoria));
+		Path filePath = Paths.get("phrases.txt");
+
+		try {
+			List<String> phrases = Files.readAllLines(filePath);
+			int sortedPhrase = (int) (Math.random() * phrases.size());
+			System.out.println("E a frase do dia é: " + phrases.get(sortedPhrase));
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
